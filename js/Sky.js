b200f52cc8a27c6cee41a9aa64a88866f383bdc7
王文成 <dcz6666@126.com>
2023-10-06 16:59:09 +0800

    '添加子hero类'

---
+++ js/Sky.js
@@ -0,0 +1,9 @@
+//景天属性& 动态方法两个部分
+//静态属性提供一个叫constructor函数中 构造器函数 每当你 new xxx() 你总会先调用这个constructor（）函数
+class Sky{
+    constructor(){
+        //静态属性
+        
+    }
+
+}
---
+++ js/config.js
@@ -0,0 +1,7 @@
+//天空类配置
+const SkyConfig={
+    bg:bg,
+    width:480,
+    height:650,
+    speed:10
+}
